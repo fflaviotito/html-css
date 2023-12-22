a8ec5c6f099d77285c256f648741b6cf636e7723
fflaviotito <fflavio.tito@gmail.com>
2023-12-22 09:37:50 -0300

    css update
    Atualizada as class dos botões das tarefas

--- projetos/to-do-list/js/script.js
+++ projetos/to-do-list/js/script.js
@@ -1,10 +1,9 @@
 var editForm = document.getElementsByClassName('edit-form') [0]
 editForm.style.display = "block"
 
-var id_ButtonListItem = []
-var quant_ButtonListItem = 0
+var quantList = 0
 
-// Function que irá rodar quando o botão de adicionar tarefa for apertado
+// Defini o que será execitado, de acordo com o botão
 document.addEventListener('click', (event) => {
     if (event.target.matches('#add-button')){
         adicionar()
@@ -12,8 +11,6 @@ document.addEventListener('click', (event) => {
 })
 
 function adicionar() {
-    id_ButtonListItem.push(quant_ButtonListItem)
-    quant_ButtonListItem++
     // Coletar o texto digitado pelo usuário
     var input = document.getElementById('add-tarefa')
     var add = input.value
@@ -26,26 +23,29 @@ function adicionar() {
     var corpo = document.getElementById('corpo')
     var newListItem = document.createElement('div')
     newListItem.classList.add(`list-item`)
-    newListItem.id = `list-item${quant_ButtonListItem}`
+    newListItem.classList.add(`list-${quantList}`)
+    newListItem.id = `list-item`
     corpo.appendChild(newListItem)
     // Adicionar o título da tarefa na nova divisão
     var title_newListItem = document.createElement('h2')
     title_newListItem.classList.add('list-title')
-    title_newListItem.id = `list-title${quant_ButtonListItem}`
+    title_newListItem.classList.add(`list-${quantList}`)
+    title_newListItem.id = `list-title`
     title_newListItem.innerHTML = add
     newListItem.appendChild(title_newListItem)
     //Adicionar os botões na nova divisão
     var button = ["check", "edit", "delete"]
     for (var c = 0; c < 3; c++) {
         var newButton = document.createElement('button')
-        newButton.classList.add('button-icons')
-        newButton.id = `list-${button[c]}-button${quant_ButtonListItem}`
+        newButton.classList.add('list-button-icons')
+        newButton.classList.add(`list-${quantList}`)
+        newButton.id = `list-${button[c]}-button`
         var newIcon = document.createElement('span')
         newIcon.classList.add('material-symbols-outlined')
         newIcon.innerHTML = `${button[c]}`
         newButton.appendChild(newIcon)
         newListItem.appendChild(newButton)
     }
-    console.log(`var ${id_ButtonListItem}`)
-    console.log(`tamanho ${id_ButtonListItem.length}`)
+    quantList++
+    console.log(quantList)
 }
